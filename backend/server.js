81f8a84458c8f1c76e3876125b20b6e2e3aea4be
shivakumargunda <shivakumargunda11@gmail.com>
2026-06-03 21:30:53 +0530

    Specify backend entrypoint in vercel.json and export app in server.js

--- backend/server.js
+++ backend/server.js
@@ -48,3 +48,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
 });
+
+export default app;
